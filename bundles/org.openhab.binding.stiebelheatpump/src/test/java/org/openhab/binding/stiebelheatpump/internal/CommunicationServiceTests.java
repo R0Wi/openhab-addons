8901bbac158f2ea7e904bc6a6d674caf7c25f54c
Robin Windey <ro.windey@gmail.com>
2024-06-23 15:04:28 +0200

    Temp: add display call and tests

--- bundles/org.openhab.binding.stiebelheatpump/src/test/java/org/openhab/binding/stiebelheatpump/internal/CommunicationServiceTests.java
+++ bundles/org.openhab.binding.stiebelheatpump/src/test/java/org/openhab/binding/stiebelheatpump/internal/CommunicationServiceTests.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2010-2023 Contributors to the openHAB project
+ * Copyright (c) 2010-2024 Contributors to the openHAB project
  *
  * See the NOTICE file(s) distributed with this work for additional
  * information.
@@ -16,21 +16,32 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 import org.openhab.binding.stiebelheatpump.protocol.DataParser;
 import org.openhab.binding.stiebelheatpump.protocol.ProtocolConnector;
 import org.openhab.binding.stiebelheatpump.protocol.RecordDefinition;
 import org.openhab.binding.stiebelheatpump.protocol.RecordDefinition.Type;
+import org.openhab.binding.stiebelheatpump.protocol.Request;
 import org.openhab.core.io.transport.serial.SerialPort;
 import org.openhab.core.io.transport.serial.SerialPortIdentifier;
 import org.openhab.core.io.transport.serial.SerialPortManager;
 import org.openhab.core.util.HexUtils;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.FrameworkUtil;
 
 /**
  * @author Stefan Triller
@@ -68,6 +79,52 @@ public class CommunicationServiceTests {
         String correctAnswer = "01007C0A056C1003";
 
         assertEquals(req, correctAnswer);
+    }
+
+    @Test
+    public void testParseLwzThz504() throws StiebelHeatPumpException, IOException {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        URL congfigUrl = classLoader.getResource("HeatpumpConfig/LWZ_THZ504_7_59.xml");
+        Bundle mockedBundle = mock(Bundle.class);
+        when(mockedBundle.getEntry(anyString())).thenReturn(congfigUrl);
+        try (MockedStatic<FrameworkUtil> mockedFrameworkUtil = Mockito.mockStatic(FrameworkUtil.class)) {
+            mockedFrameworkUtil.when(() -> FrameworkUtil.getBundle(ConfigParser.class)).thenReturn(mockedBundle);
+            ConfigLocator configLocator = new ConfigLocator("LWZ_THZ504_7_59.xml");
+            List<Request> requests = configLocator.getRequests();
+            // Value Response
+            // String responseHexStr =
+            // "010096fbfda8005301ad017b023001a78001fda800d50176100807012c012c02ee000d00190019006e014f000002f00a5e45393c4944e12734062500cc00290000017d019a008d009f0183000000000af6";
+
+            // FB = Value Request, 09 = Static settings Request, 17 = temp settings Request, FD = Get Version, 0A0176 =
+            // Get Display
+            String requestString = "0A0176";
+            String responseHexStr = "0100990a01760413";
+
+            // Auto, Compressor, DWH, Heatpump => 1; Booster, Heating, Defrost => 0: 0100990a01760413
+
+            // Auto, all others off: 0100830a01760001
+
+            // all off: 0100820a01760000
+
+            byte[] response = HexUtils.hexToBytes(responseHexStr);
+
+            byte requestByte = HexUtils.hexToBytes(requestString)[0];
+            Request request = requests.stream().filter(r -> r.getRequestByte()[0] == requestByte).findFirst().get();
+
+            DataParser dataParser = new DataParser();
+            Map<String, Object> parsedRecords = dataParser.parseRecords(response, request);
+
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter("parsedRecords.txt"))) {
+                // Print values
+                parsedRecords.forEach((key, value) -> {
+                    try {
+                        writer.write(key + " = " + value + "\n");
+                    } catch (IOException e) {
+                        // e.printStackTrace();
+                    }
+                });
+            }
+        }
     }
 
     private SerialPort mockSerialPort() throws Exception {
