a4466c2afdb8c17f0ed7dc36d3a69a39cdb3307f
Robin Windey <ro.windey@gmail.com>
2024-06-23 15:04:28 +0200

    Temp: add display call and tests

---
+++ bundles/org.openhab.binding.stiebelheatpump/build-deploy.sh
@@ -0,0 +1,2 @@
+mvn clean install -DskipTests karaf:kar # && \
+#scp target/*.kar robin@testvm-ubuntu:~/openhab/addons
--- bundles/org.openhab.binding.stiebelheatpump/src/main/java/org/openhab/binding/stiebelheatpump/internal/CommunicationService.java
+++ bundles/org.openhab.binding.stiebelheatpump/src/main/java/org/openhab/binding/stiebelheatpump/internal/CommunicationService.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2010-2023 Contributors to the openHAB project
+ * Copyright (c) 2010-2024 Contributors to the openHAB project
  *
  * See the NOTICE file(s) distributed with this work for additional
  * information.
@@ -69,6 +69,7 @@ public class CommunicationService {
         try {
             connector.connect(serialPortManager, serialPortName, baudRate);
         } catch (StiebelHeatPumpException e) {
+            logger.error("Could not connect to heatpump", e);
         }
     }
 
