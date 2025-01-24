febc1caa6c32d16eb7261835de45154db03ff6c5
Robin Windey <ro.windey@gmail.com>
2025-01-24 18:10:28 +0100

    Compat Openhab 4.3.2

--- bundles/org.openhab.binding.stiebelheatpump/build-deploy.sh
+++ bundles/org.openhab.binding.stiebelheatpump/build-deploy.sh
@@ -1,9 +1,6 @@
 #mvn clean install -DskipTests karaf:kar && \
 #scp target/*.kar robin@testvm-ubuntu:~/openhab/addons
 mvn clean install -DskipTests && \
-#scp target/org.openhab.binding.stiebelheatpump-4.2.1.jar robin@server.windey.home:/home/robin/docker/openhab_data/addons/kar-extract/repository/org/openhab/addons/bundles/org.openhab.binding.stiebelheatpump/4.2.1/org.openhab.binding.stiebelheatpump-4.2.1.jar
-scp target/org.openhab.binding.stiebelheatpump-4.2.1.jar robin@server.windey.home:/home/robin/docker/openhab_data/addons/org.openhab.binding.stiebelheatpump-4.2.1.jar
+scp target/org.openhab.binding.stiebelheatpump-4.3.2.jar robin@server.windey.home:/home/robin/docker/openhab_data/addons/manual-bundles/org.openhab.binding.stiebelheatpump.jar
 
-#bundle:uninstall <id>
-#bundle:install file:/openhab/addons/kar-extract/repository/org/openhab/addons/bundles/org.openhab.binding.stiebelheatpump/4.2.1/org.openhab.binding.stiebelheatpump-4.2.1.jar
-#bundle:start <id>
+# bundle:update "openHAB Add-ons :: Bundles :: stiebelheatpump Binding" file:/openhab/addons/manual-bundles/org.openhab.binding.stiebelheatpump.jar
